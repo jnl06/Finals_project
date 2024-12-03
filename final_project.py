f8d5521764ea4eee9d1f72df6286d09a361b4f0a
Jonel <jonelpronton06@gmail.com>
2024-12-03 15:47:51 +0800

    Add files via upload

--- final_project.py
+++ final_project.py
@@ -18,6 +18,7 @@ from activity15 import activity15
 from activity16 import activity16
 from activity17 import activity17
 from activity18 import activity18
+
 #from activity19 import activity19
 #from activity20 import activity20
 #from activity21 import activity21
@@ -46,105 +47,99 @@ print("==============================\n")
 name = input("Please enter your name: ")
 print("-----------------------------\n")
 print(f"Hello {name}")
-choice = input("What would you like to open?\n1 - Activities \n2 - Code Challenges \nYour answer:")
+
 con = True
 
 while con:
+    choice = input("\nWhat would you like to open?\n1 - Activities \n2 - Code Challenges \n0 - Exit\nYour answer: ")
+    
     if choice == "1":
-        act_num = input("Select activity to open: \n1 - ")
-        if act_num == "1":
-            activity1()
-            continue
+        while True:
+            act_num = input("\nSelect activity to open (Enter 0 to return to main menu): ")
             
-        elif act_num == "2":
-            activity2()
-            continue
+            if act_num == "1":
+                activity1()
+            elif act_num == "2":
+                activity2()
+            elif act_num == "3":
+                activity3()
+            elif act_num == "4":
+                activity4()
+            elif act_num == "5":
+                activity5()
+            elif act_num == "6":
+                activity6()
+            elif act_num == "7":
+                activity7()
+            elif act_num == "8":
+                activity8()
+            elif act_num == "9":
+                activity9()
+            elif act_num == "10":
+                activity10()
+            elif act_num == "11":
+                activity11()
+            elif act_num == "12":
+                activity12()
+            elif act_num == "13":
+                activity13()
+            elif act_num == "14":
+                activity14()
+            elif act_num == "15":
+                activity15()
+            elif act_num == "16":
+                activity16()
+            elif act_num == "17":
+                activity17()
+            elif act_num == "18":
+                activity18()
+            elif act_num == "0":
+                break  # Return to main menu
+            else:
+                print("Invalid choice. Please try again.")
+
+    elif choice == "2":
+        while True:
+            chal_num = input("\nSelect code challenge to open (Enter 0 to return to main menu): ")
             
-        elif act_num == "3":
-            activity3()
-            continue    
-            
-        elif act_num == "4":
-            activity4()
-            continue
-            
-        elif act_num == "5":
-            activity5()
-            continue
-            
-        elif act_num == "6":
-            activity6()
-            continue
-            
-        elif act_num == "7":
-            activity7()
-            continue
-            
-        elif act_num == "8":
-            activity8()
-            continue
-            
-        elif act_num == "9":
-            activity9()
-            continue
-            
-        elif act_num == "10":
-            activity10()
-            continue
-            
-        elif act_num == "11":
-            activity11()
-            continue
-            
-        elif act_num == "12":
-            activity12()
-            continue
-            
-        elif act_num == "13":
-            activity13()
-            continue
-            
-        elif act_num == "14":
-            activity14()
-            continue
-            
-        elif act_num == "15":
-            activity15()
-            continue
-            
-        elif act_num == "16":
-            activity16()
-            continue
-            
-        elif act_num == "17":
-            activity17()
-            continue
-            
-        elif act_num == "18":
-            activity18()
-            continue
-            
-        elif act_num == "19":
-            activity19()
-            continue
-            
-        elif act_num == "20":
-            activity20()
-            continue
-            
-        elif act_num == "21":
-            activity21()
-            continue
-            
-        elif act_num == "22":
-            activity22()
-            continue
-            
-        elif act_num == "0":
-            return choice
-            
-        else:
-            print("Invalid Choice")
-            break
-            
-            
+            if chal_num == "1":
+                code_chal1()
+            elif chal_num == "2":
+                code_chal2()
+            elif chal_num == "4":
+                code_chal4()
+            elif chal_num == "5":
+                code_chal5()
+            elif chal_num == "6":
+                code_chal6()
+            elif chal_num == "7":
+                code_chal7()
+            elif chal_num == "8":
+                code_chal8()
+            elif chal_num == "9":
+                code_chal9()
+            elif chal_num == "10":
+                code_chal10()
+            elif chal_num == "11":
+                code_chal11()
+            elif chal_num == "12":
+                code_chal12()
+            elif chal_num == "13":
+                code_chal13()
+            elif chal_num == "14":
+                code_chal14()
+            elif chal_num == "15":
+                code_chal15()
+            elif chal_num == "16":
+                code_chal16()
+            elif chal_num == "0":
+                break 
+            else:
+                print("Invalid choice. Please try again.")
+    
+    elif choice == "0":
+        print("\nExiting the program. Goodbye!")
+        con = False 
+    
+    else:
+        print("Invalid choice. Please select again.")
