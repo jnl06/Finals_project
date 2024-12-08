fedea858ac8bc6567db0cfa2908c7b346e59437e
Jonel <jonelpronton06@gmail.com>
2024-12-08 09:27:36 +0800

    Delete code_challenge5.py

--- code_challenge5.py
+++
@@ -1,5 +0,0 @@
-def code_chal5():
-    fahrenheit = eval(input("Enter a temperature in fahrenheit to convert into celsius: "))
-    celsius = ((fahrenheit - 32) * 5) / 9
-
-    print(f"{fahrenheit}°f is converted into {round(celsius, 2)}°C")
