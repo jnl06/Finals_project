8dd68516d72a097c74b737cb5e64a89712e3431d
Jonel <jonelpronton06@gmail.com>
2024-12-08 09:27:57 +0800

    Delete code_challenge2.py

--- code_challenge2.py
+++
@@ -1,3 +0,0 @@
-def code_chal2():
-    name = input("What is your name? ")
-    print("\t\t\t\t\t\t\t\t\t\t\t\t      * \n\t\t\t\t\t\t\t\t\t\t\t\t    * * * \n\t\t\t\t\t\t\t\t\t\t\t\t  * * * * * \n\t\t\t\t\t\t\t\t\t\t\t\t*Hi, " + name + "* \n\t\t\t\t\t\t\t\t\t\t\t\t  * * * * * \n\t\t\t\t\t\t\t\t\t\t\t\t    * * * \n\t\t\t\t\t\t\t\t\t\t\t\t      *")
