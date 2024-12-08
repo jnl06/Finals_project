6bdf501c404120213c00a55e15d4c6e9b9eefec3
Jonel <jonelpronton06@gmail.com>
2024-12-08 09:31:32 +0800

    Delete activity17.py

--- activity17.py
+++
@@ -1,7 +0,0 @@
-def activity17():
-    col = eval(input("Enter number of colums: "))
-
-    for x in range(1, 11):
-        for y in range(1, col + 1):
-            print(f"{x} x {y} = {x * y}", end = "\t")
-        print()    
