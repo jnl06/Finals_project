764b1bfc0c8d6c673219922087443c548f640ead
Jonel <jonelpronton06@gmail.com>
2024-12-08 09:32:14 +0800

    Delete activity13.py

--- activity13.py
+++
@@ -1,7 +0,0 @@
-def activity13():
-    n1 = eval(input("Enter a number: "))
-    n2 = 1
-    for x in range(n1, 0, -1):
-        print(x)
-        n2 *= x
-    print(f"The Factorial of {n1} is {n2}")
