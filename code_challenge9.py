e89a48a8c251a7db4cb816b2bd6b0bcdc120f578
Jonel <jonelpronton06@gmail.com>
2024-12-08 09:26:48 +0800

    Delete code_challenge9.py

--- code_challenge9.py
+++
@@ -1,7 +0,0 @@
-def code_chal9():
-    for x in range(0, 11):
-        for y in range(x):
-            print(" ", end = " ")
-        for z in range(x, 11):
-            print("*", end = " ")
-        print()
