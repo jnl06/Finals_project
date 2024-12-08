6abae3dc220518f370f05021f914df019b694167
Jonel <jonelpronton06@gmail.com>
2024-12-08 09:30:27 +0800

    Delete activity5.py

--- activity5.py
+++
@@ -1,9 +0,0 @@
-def activity5():
-    print("Fahrenheit to Celsius Converter")
-    print("===============================")
-
-    fh = eval(input("Enter temperature in fahreheit: "))
-
-    cl = ((fh - 32) * 5) / 9
-
-    print(f"{fh} degreed fahreheit converted to celsius is {round(cl, 2)}")
